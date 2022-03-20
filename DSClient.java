7bea9bd11f374971371ff644a0bf67c6b6f0e890
Nathan Ho <nathanho21@gmail.com>
2022-03-20 22:32:34 +1100

    implemented handshake protocol

--- DSClient.java
+++ DSClient.java
@@ -1,29 +1,55 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
 
 public class DSClient {
+    public static void handshake(BufferedReader input, DataOutputStream output) {
+        try {
+            output.write(("HELO\n").getBytes());
+            output.flush();
+            System.out.println("Client >\t" + "HELO");
+
+            String sResponse = input.readLine(); //expect "OK"
+            System.out.println("Server >\t" + sResponse);
+
+            String username = "Nathan";
+            output.write(("AUTH " + username + "\n").getBytes());
+            output.flush();
+            System.out.println("Client >\t" + "AUTH " + username);
+
+            sResponse = input.readLine(); //expect "OK"
+            System.out.println("Server >\t" + sResponse);
+
+            output.write(("REDY\n").getBytes());
+            output.flush();
+            System.out.println("Client >\t" + "REDY");
+
+            sResponse = input.readLine(); //expect jobs
+            System.out.println("Server >\t" + sResponse);
+
+            output.write(("QUIT\n").getBytes());
+            output.flush();
+            System.out.println("Client >\t" + "QUIT");
+
+            sResponse = input.readLine(); //expect "QUIT"
+            System.out.println("Server >\t" + sResponse);
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        
+    }
     public static void main(String args[]) {
         try {
-            Socket s = new Socket("localhost", 6666);
-            DataInputStream dIn = new DataInputStream(s.getInputStream());
+            Socket s = new Socket("localhost", 50000);
+            BufferedReader dIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print(">\t");
-            String userIn = scanner.nextLine();
-
-            dOut.writeUTF(userIn);
-            dOut.flush();
-            
-            String sResponse = dIn.readUTF();
-            System.out.println(">\t" + sResponse);
+            handshake(dIn, dOut);
 
             dIn.close();
+            dOut.close();
             s.close();
-            scanner.close();
-            s.close();
-            scanner.close();
         }
         catch (Exception e) {
         }
