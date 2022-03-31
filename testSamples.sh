bcefb9d1b1d627b12b330ef0244be4ee5758457b
Nathan Ho <nathanho21@gmail.com>
2022-03-31 22:35:36 +1100

    create log + desc

--- testSamples.sh
+++ testSamples.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-
+# This file will compile the Java implementation of DSClient.
+# This test file runs the reference implementation of ds-sim and the java implementation of DSClient using all given config files and outputs the results into log files.
+# The differences between the reference and Java implementation are displayed in 'log/diff.log'
 testFiles="./config"
 dsSim="./pre-compiled"
-log="./log"
 
-if test -f "$log/ds.log" | test -f "$log/client.log" | test -f "$log/diff.log"; then
-    rm log/ds.log log/client.log log/diff.log
+if ! test -d "log"; then
+    mkdir log
+fi
+l="./log"
+
+if test -f "$l/dsRef.log" | test -f "$l/client.log" | test -f "$l/diff.log"; then
+    rm $l/dsRef.log $l/client.log $l/diff.log
 fi
 
-touch log/ds.log log/client.log log/diff.log
+touch $l/dsRef.log $l/client.log $l/diff.log
+
+javac COMP3100Project46461019/DSClient.java COMP3100Project46461019/ServerInfo.java COMP3100Project46461019/JobInfo.java
 
 for f in $testFiles/*.xml; do 
     echo "running server and client with $f"
-    $dsSim/ds-server -c $f -v brief >> log/ds.log & sleep 2
+    $dsSim/ds-server -c $f -v brief >> $l/dsRef.log & sleep 2
     $dsSim/ds-client -a lrr
 
-    $dsSim/ds-server -c $f -v brief -n >> log/client.log & sleep 2
+    $dsSim/ds-server -c $f -v brief -n >> $l/client.log & sleep 2
     java COMP3100Project46461019.DSClient -lrr
     echo "done running with $f"
 done
 
-diff log/ds.log log/client.log > log/diff.log
+diff $l/dsRef.log $l/client.log > $l/diff.log
