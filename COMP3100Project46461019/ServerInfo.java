5bc40e51fc60dccb0c071f9b903ba7ad57f180cb
Nathan Ho <nathanho21@gmail.com>
2022-05-13 16:05:48 +1000

    + available resources + setters

--- COMP3100Project46461019/ServerInfo.java
+++ COMP3100Project46461019/ServerInfo.java
@@ -8,6 +8,9 @@ public class ServerInfo {
     int core;
     int memory;
     int disk;
+    int availCore;
+    int availMem;
+    int availDisk;
     int wJobs; //Jobs waiting
     int rJobs; //Jobs running
 
@@ -23,6 +26,9 @@ public class ServerInfo {
         core = Integer.parseInt(info[4]);
         memory = Integer.parseInt(info[5]);
         disk = Integer.parseInt(info[6]);
+        availCore = core;
+        availMem = memory;
+        availDisk = disk;
         wJobs = Integer.parseInt(info[7]);
         rJobs = Integer.parseInt(info[8]);
     }
@@ -52,6 +58,30 @@ public class ServerInfo {
     }
 
     /**
+     * Sets the available cores of server after taking a job
+     * @param taken - the amount of cores taken by a job
+     */
+    public void setAvailCores(int taken) {
+        this.availCore = this.availCore - taken;
+    }
+
+    /**
+     * Sets the available memory of server after taking a job
+     * @param taken - the amount of memory taken by a job
+     */
+    public void setAvailMem(int taken) {
+        this.availMem = this.availMem - taken;
+    }
+
+    /**
+     * Sets the available disk of server after taking a job
+     * @param taken - the amount of disk taken by a job
+     */
+    public void setAvailDisk(int taken) {
+        this.availDisk = this.availDisk - taken;
+    }
+
+    /**
      * Print the server information
      */
     public void printServer() {
@@ -63,6 +93,11 @@ public class ServerInfo {
         System.out.print(this.core + " ");
         System.out.print(this.memory + " ");
         System.out.print(this.disk + " ");
+        System.out.print(this.availCore + " ");
+        System.out.print(this.availMem + " ");
+        System.out.print(this.availDisk + " ");
+        System.out.print(this.wJobs + " ");
+        System.out.print(this.rJobs + " ");
         System.out.println();
     }
 }
