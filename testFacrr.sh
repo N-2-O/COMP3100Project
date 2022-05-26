85eac9442875d14aaa060f8c94ccf5acc5658f06
Nathan Ho <nathanho21@gmail.com>
2022-05-26 11:36:58 +1000

    + compile cmd

--- testFacrr.sh
+++ testFacrr.sh
@@ -1,1 +1,2 @@
+javac COMP3100Project46461019/DSClient.java COMP3100Project46461019/ServerInfo.java COMP3100Project46461019/JobInfo.java
 ./stage2-test-x86 "java COMP3100Project46461019.DSClient -facrr" -o tt -n
